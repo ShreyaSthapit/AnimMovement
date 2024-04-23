2bc1f4cc928982e137fca9ee173ff136a81e3cb0
ShreyaSthapit <np03cs4a230055@heraldcollege.edu.np>
2024-04-23 19:32:23 +0545

    Animation in Character.
    Animation movement in character.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 1;
+    [SerializeField]
+    private float lookSensitivity = 5;
+    [SerializeField]
+    private float jumpHeight = 10;
+    [SerializeField]
+    private float gravity = 9.81f;
+    private Vector2 moveVector;
+    private Vector2 lookVector;
+    private Vector3 rotation;
+    private float verticalVelocity;
+    private CharacterController characterController;
+    private Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveVector = context.ReadValue<Vector2>();
+        if (moveVector.magnitude > 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+    private void Move()
+    {
+        verticalVelocity += -gravity * Time.deltaTime;
+        if (characterController.isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -0.1f * gravity * Time.deltaTime;
+        }
+        Vector3 move = transform.right * moveVector.x + transform.forward * moveVector.y + transform.up * verticalVelocity;
+        characterController.Move(move * moveSpeed * Time.deltaTime);
+
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookVector = context.ReadValue<Vector2>();
+
+    }
+
+    private void Rotate()
+    {
+        rotation.y += lookVector.x * lookSensitivity * Time.deltaTime;
+        transform.localEulerAngles = rotation;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (characterController.isGrounded && context.performed)
+        {
+            animator.Play("Jumping");
+            // Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        verticalVelocity = Mathf.Sqrt(jumpHeight * gravity);
+    }
+}
